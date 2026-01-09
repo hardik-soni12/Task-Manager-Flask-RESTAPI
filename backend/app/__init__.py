04f65fecf054cae38aaa12643d11671a8a23e333
Hardik-Soni12 <imailhr1k@gmail.com>
2026-01-09 21:06:49 +0530

    feat: implement User model with bcrypt hashing and server-side timestamps

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from .extensions import db, bcrypt, jwt, migrate
+from backend.config import Config_dict
 
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(Config_dict[config_name])
 
+    # Initialize extentions
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
 
     return app
---
+++ backend/app/models/user.py
@@ -0,0 +1,23 @@
+from sqlalchemy import func
+from ..extensions import db, bcrypt
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100), nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    password = db.Column(db.String(250), nullable = False)
+    role = db.Column(db.String(20), default='user')
+    created_at = db.Column(db.DateTime, server_default = func.now())
+
+    # password hashing using bcrypt
+    def set_password(self, raw_password):
+        self.password = bcrypt.generate_password_hash(raw_password).decode('utf-8')
+
+    def check_password(self, raw_password):
+        return bcrypt.check_password_hash(self.password, raw_password)
+    
+    def __repr__(self):
+        return f'User {self.email}'
